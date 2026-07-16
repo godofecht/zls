b9aefba055aa3d1e4b306a9db01e60b9463581b7
Techatrix <techatrix@mailbox.org>
2026-07-16 07:46:12 +0200

    do not force lld as linker when using -Duse-llvm

--- build.zig
+++ build.zig
@@ -170,7 +170,6 @@ pub fn build(b: *Build) !void {
                 .root_module = exe_module,
                 .max_rss = if (optimize == .Debug and target_query.os_tag == .wasi) 2_600_000_000 else 2_000_000_000,
                 .use_llvm = use_llvm,
-                .use_lld = use_llvm,
             });
         }
 
@@ -212,7 +211,6 @@ pub fn build(b: *Build) !void {
             .name = "zls",
             .root_module = exe_module,
             .use_llvm = use_llvm,
-            .use_lld = use_llvm,
         });
         b.installArtifact(exe);
     }
@@ -241,7 +239,6 @@ pub fn build(b: *Build) !void {
         }),
         .filters = test_filters,
         .use_llvm = use_llvm,
-        .use_lld = use_llvm,
     });
 
     const src_tests = b.addTest(.{
@@ -249,7 +246,6 @@ pub fn build(b: *Build) !void {
         .root_module = zls_module,
         .filters = test_filters,
         .use_llvm = use_llvm,
-        .use_lld = use_llvm,
     });
 
     blk: { // zig build test, zig build test-build-runner, zig build test-analysis
